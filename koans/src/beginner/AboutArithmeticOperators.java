16f88a5a04e793ef6d4dfe9b4bae81df7bc78613
Muhidin <muhidinhukic1@gmail.com>
2021-02-23 13:53:22 -0600

    strings, asssertions, arithmetic operators JAVA

--- koans/src/beginner/AboutArithmeticOperators.java
+++ koans/src/beginner/AboutArithmeticOperators.java
@@ -6,52 +6,39 @@ import static com.sandwich.koan.constant.KoanConstants.__;
 import static com.sandwich.util.Assert.assertEquals;
 
 public class AboutArithmeticOperators {
-
-    @Koan
-    public void simpleOperations() {
-        assertEquals(1, __);
-        assertEquals(1 + 1, __);
-        assertEquals(2 + 3 * 4, __);
-        assertEquals((2 + 3) * 4, __);
-        assertEquals(2 * 3 + 4, __);
-        assertEquals(2 - 3 + 4, __);
-        assertEquals(2 + 4 / 2, __);
-        assertEquals((2 + 4) / 2, __);
-    }
-
     @Koan
     public void notSoSimpleOperations() {
-        assertEquals(1 / 2, __);
-        assertEquals(3 / 2, __);
-        assertEquals(1 % 2, __);
-        assertEquals(3 % 2, __);
+        assertEquals(1 / 2, 0);
+        assertEquals(3 / 2, 1);
+        assertEquals(1 % 2, 1);
+        assertEquals(3 % 2, 1);
     }
 
     @Koan
     public void minusMinusVariableMinusMinus() {
         int i = 1;
-        assertEquals(--i, __);
-        assertEquals(i, __);
-        assertEquals(i--, __);
-        assertEquals(i, __);
+        assertEquals(--i, 0);
+        assertEquals(i, 0);
+        assertEquals(i--, 0);
+        assertEquals(i, -1);
     }
 
     @Koan
     public void plusPlusVariablePlusPlus() {
         int i = 1;
-        assertEquals(++i, __);
-        assertEquals(i, __);
-        assertEquals(i++, __);
-        assertEquals(i, __);
+        assertEquals(++i, 2);
+        assertEquals(i, 2);
+        assertEquals(i++, 2);
+        assertEquals(i, 3);
     }
 
     @Koan
     public void timesAndDivInPlace() {
         int i = 1;
         i *= 2;
-        assertEquals(i, __);
+        assertEquals(i, 2);
         i /= 2;
-        assertEquals(i, __);
+        assertEquals(i, 1);
     }
 
 }
